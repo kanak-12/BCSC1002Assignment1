174a1925bf30e676407d04b68161ded155642f4a
Kanak-12 <kanak.srivastava_cs19@gla.ac.in>
2020-08-30 18:15:39 +0530

    Add getters and setters in Student.Java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -13,4 +13,52 @@ public class Student {
     private long universityRollNumberOfStudent;
     private int numberOfBooksIssuedByStudent;
     private Book[] namesOfBookIssuedByStudent;
+
+    public int getNumberOfBooksIssuedByStudent() {
+        return numberOfBooksIssuedByStudent;
+    }
+
+    public void setNumberOfBooksIssuedByStudent(int numberOfBooksIssuedByStudent) {
+        this.numberOfBooksIssuedByStudent = numberOfBooksIssuedByStudent;
+    }
+
+    public long getUniversityRollNumberOfStudent() {
+        return universityRollNumberOfStudent;
+    }
+
+    public void setUniversityRollNumberOfStudent(long universityRollNumberOfStudent) {
+        this.universityRollNumberOfStudent = universityRollNumberOfStudent;
+    }
+
+    public String getFirstNameOfStudent() {
+        return firstNameOfStudent;
+    }
+
+    public void setFirstNameOfStudent(String firstNameOfStudent) {
+        this.firstNameOfStudent = firstNameOfStudent;
+    }
+
+    public String getLastNameOfStudent() {
+        return lastNameOfStudent;
+    }
+
+    public void setLastNameOfStudent(String lastNameOfStudent) {
+        this.lastNameOfStudent = lastNameOfStudent;
+    }
+
+    public String getMiddleNameOfStudent() {
+        return middleNameOfStudent;
+    }
+
+    public void setMiddleNameOfStudent(String middleNameOfStudent) {
+        this.middleNameOfStudent = middleNameOfStudent;
+    }
+
+    public Book[] getNamesOfBookIssuedByStudent() {
+        return namesOfBookIssuedByStudent;
+    }
+
+    public void setNamesOfBookIssuedByStudent(Book[] namesOfBookIssuedByStudent) {
+        this.namesOfBookIssuedByStudent = namesOfBookIssuedByStudent;
+    }
 }
