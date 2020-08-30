0fc4102c02301718c3c7aaa3f676bb977ead35c6
Kanak-12 <kanak.srivastava_cs19@gla.ac.in>
2020-08-30 17:46:06 +0530

    Add getters and setters in Book.Java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -10,4 +10,29 @@ public class Book {
     private String nameOfBook;
     private String authorOfBook;
     private String isbnNumber;
+
+    public String getAuthorOfBook() {
+        return authorOfBook;
+    }
+
+    public void setAuthorOfBook(String authorOfBook) {
+        this.authorOfBook = authorOfBook;
+    }
+
+    public String getNameOfBook() {
+        return nameOfBook;
+    }
+
+    public void setNameOfBook(String nameOfBook) {
+        this.nameOfBook = nameOfBook;
+    }
+
+    public String getIsbnNumber() {
+        return isbnNumber;
+    }
+
+    public void setIsbnNumber(String isbnNumber) {
+        this.isbnNumber = isbnNumber;
+    }
+
 }
