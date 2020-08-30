839eb1ab37f2fb47666dab1e55cb513f57c7824e
Kanak-12 <kanak.srivastava_cs19@gla.ac.in>
2020-08-30 21:48:00 +0530

    Add getter and setter for private field in Library.Java

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -1,5 +1,5 @@
 /*  Created by IntelliJ IDEA.
- *  User: Kanak Srivastva (kanak-12)
+ *  User: Kanak Srivastava (kanak-12)
  *  Date: 29/08/20
  *  Time: 10:00 AM
  *  File Name : Library.java
@@ -8,4 +8,13 @@ package definitions;
 
 public class Library {
     private Book[] booksThatAreCurrentlyAvailabe;
+
+    public Book[] getBooksThatAreCurrentlyAvailabe() {
+        return booksThatAreCurrentlyAvailabe;
+    }
+
+    public void setBooksThatAreCurrentlyAvailabe(Book[] booksThatAreCurrentlyAvailabe) {
+        this.booksThatAreCurrentlyAvailabe = booksThatAreCurrentlyAvailabe;
+    }
 }
+
