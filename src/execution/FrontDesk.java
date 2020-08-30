e6fd2d944924b4518d970b040d4cd3a4794cced4
Kanak-12 <kanak.srivastava_cs19@gla.ac.in>
2020-08-30 22:47:38 +0530

    Add constant values to issue a book ,return a book ,show all issued books and exit in FrontDesk.java

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -1,12 +1,17 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:50 PM
+ *  User: Kanak Srivastava (kanak-12)
+ *  Date: 29/08/20
+ *  Time: 2:45 PM
  *  File Name : FrontDesk.java
  * */
 package execution;
 
 public class FrontDesk {
+    private static final int ISSUE_BOOK = 1;
+    private static final int RETURN_BOOK = 2;
+    private static final int SHOW_ALL_ISSUED_BOOKS = 3;
+    private static final int EXIT = 4;
+
     public static void main(String[] args) {
 
     }
