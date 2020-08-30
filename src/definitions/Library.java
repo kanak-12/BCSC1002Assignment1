d2e71da8f445f3c78aeabc9a2926c6a51347c6a2
Kanak-12 <kanak.srivastava_cs19@gla.ac.in>
2020-08-30 21:45:32 +0530

    Add private field in Library.Java

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -1,10 +1,11 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:50 PM
+ *  User: Kanak Srivastva (kanak-12)
+ *  Date: 29/08/20
+ *  Time: 10:00 AM
  *  File Name : Library.java
  * */
 package definitions;
 
 public class Library {
+    private Book[] booksThatAreCurrentlyAvailabe;
 }
