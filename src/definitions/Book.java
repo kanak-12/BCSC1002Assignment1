6410b1247097489a6b89ddd268bfad820d64b9b3
Kanak-12 <kanak.srivastava_cs19@gla.ac.in>
2020-08-30 17:30:14 +0530

    Add private fields in Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -1,10 +1,13 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:49 PM
+ *  User: Kanak Srivastava (kanak-12)
+ *  Date: 28/08/20
+ *  Time: 8:06 PM
  *  File Name : Book.java
  * */
 package definitions;
 
 public class Book {
+    private String nameOfBook;
+    private String authorOfBook;
+    private String isbnNumber;
 }
